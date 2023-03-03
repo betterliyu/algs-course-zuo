a7276658069fd402220adfc13d9ef8b65c06b9c1
liyu <edenliyu@outlook.com>
2023-03-03 17:14:27 +0800

    bitmap

---
+++ basic/class05/Code01_BitMap.java
@@ -0,0 +1,61 @@
+package basic.class05;
+
+import java.util.HashSet;
+
+public class Code01_BitMap {
+    public static class BitMap {
+        long[] bits;
+
+        public BitMap(int max) {
+            bits = new long[(max >> 6) + 1];
+        }
+
+        public void add(int num) {
+            // | 当前为 1 其他位都为 0 的数
+            // num & 63 == num % 64
+            bits[num >> 6] |= 1L << (num & 63);
+        }
+
+        public boolean contains(int num) {
+            return (bits[num >> 6] & (1L << (num & 63))) != 0;
+        }
+
+        public void delete(int num) {
+            // & 当前为 0 其他位都为 1 的数
+            bits[num >> 6] &= ~(1L << (num & 63));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("测试开始！");
+        int max = 10000;
+        BitMap bitMap = new BitMap(max);
+        HashSet<Integer> set = new HashSet<>();
+        int testTime = 10000000;
+        for (int i = 0; i < testTime; i++) {
+            int num = (int) (Math.random() * (max + 1));
+            double decide = Math.random();
+            if (decide < 0.333) {
+                bitMap.add(num);
+                set.add(num);
+            } else if (decide < 0.666) {
+                bitMap.delete(num);
+                set.remove(num);
+            } else {
+                if (bitMap.contains(num) != set.contains(num)) {
+                    System.out.println("Oops!");
+                    break;
+                }
+            }
+        }
+        for (int num = 0; num <= max; num++) {
+            if (bitMap.contains(num) != set.contains(num)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("测试结束！");
+    }
+
+
+}
